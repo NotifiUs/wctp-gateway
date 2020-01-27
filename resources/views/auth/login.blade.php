4b9c51d3b0554395ca104860b2d61656cd9cc841
Patrick Labbett <patrick@labbett.net>
2020-01-26 22:10:58 -0500

    composer update, yarn upgrade, minor login page update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -66,7 +66,7 @@
                                 </button>
 
                                 @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                    <a class="mx-3 my-3" href="{{ route('password.request') }}">
                                         {{ __('Forgot Your Password?') }}
                                     </a>
                                 @endif
