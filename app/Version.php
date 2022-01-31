bf486d0554c682b6009f1327c6a8ffc41ae37b62
Patrick Labbett <patrick@labbett.net>
2022-01-31 16:01:42 -0500

    updated version model to remove -dev designation on application version display

--- app/Version.php
+++ app/Version.php
@@ -8,7 +8,7 @@ class Version
 {
     const MAJOR = 1;
     const MINOR = 0;
-    const PATCH = 0;
+    const PATCH = 1;
 
     public static function get()
     {
@@ -16,6 +16,6 @@ class Version
 
         $commitDate = Carbon::parse(trim(exec('git log -n1 --pretty=%ci HEAD')), 'UTC');
 
-        return "wctpd " . sprintf('v%s.%s.%s-dev.%s (%s)', self::MAJOR, self::MINOR, self::PATCH, $commitHash, $commitDate->format('Y-m-d H:i:s'));
+        return "wctpd " . sprintf('v%s.%s.%s.%s (%s)', self::MAJOR, self::MINOR, self::PATCH, $commitHash, $commitDate->format('Y-m-d H:i:s'));
     }
 }
